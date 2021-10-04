48d6be29649695a793e0bf718f77a640d323cf59
Whachara nuanlong <whachara479@gmail.com>
2021-10-05 06:18:40 +0700

    ep1 - setup and static folder

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+
+func main() {
+	router := gin.Default()
+	router.Static("/images", "./uploaded/images")
+
+	//api.Setup(router)
+	router.Run(":8083")
+
+	// In case of running on Heroku
+	// var port = os.Getenv("PORT")
+	// if port == "" {
+	// 	fmt.Println("Running on Heroku using random PORT")
+	// 	router.Run()
+	// } else {
+	// 	fmt.Println("Environment Port : " + port)
+	// 	router.Run(":" + port)
+	// }
+}

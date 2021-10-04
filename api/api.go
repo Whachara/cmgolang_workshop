1751c283a2ead39df749fe5689069e2a2534db20
Whachara nuanlong <whachara479@gmail.com>
2021-10-05 06:38:49 +0700

    ep2 - group routes

---
+++ api/api.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"	
+)
+
+// Setup - call this method to setup routes
+func Setup(router *gin.Engine) {
+
+	// db.SetupDB()
+	SetupAuthenAPI(router)
+	// SetupProductAPI(router)
+	// SetupTransactionAPI(router)
+}
--- server.go
+++ server.go
@@ -1,13 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"main/api"
+	"github.com/gin-gonic/gin"
+)
 
 
 func main() {
 	router := gin.Default()
 	router.Static("/images", "./uploaded/images")
 
-	//api.Setup(router)
+	api.Setup(router)
 	router.Run(":8083")
 
 	// In case of running on Heroku

abecd43ec2c133af2e2500b4c9d6be8a56b78ea1
Whachara nuanlong <whachara479@gmail.com>
2021-10-12 10:53:03 +0700

    ep9 - etup db

--- api/authen.api.go
+++ api/authen.api.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func SetupAuthenAPI(router *gin.Engine) {
@@ -20,10 +21,20 @@ func login(c *gin.Context) {
 	c.JSON(200, gin.H{"result": "login"})
 }
 
+func checkPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+func hashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
+
 func register(c *gin.Context) {
 	var user model.User
 	if c.ShouldBind(&user) == nil {
-		// user.Password, _ = hashPassword(user.Password)
+		user.Password, _ = hashPassword(user.Password)
 		user.CreatedAt = time.Now()
 		if err := db.GetDB().Create(&user).Error; err != nil {
 			c.JSON(200, gin.H{"result": "nok", "error": err})

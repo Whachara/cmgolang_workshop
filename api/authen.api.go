bb07f5b06294d1cebdc439e55e79088297bfe799
Whachara nuanlong <whachara479@gmail.com>
2021-10-12 11:04:56 +0700

    ep10 - login

--- api/authen.api.go
+++ api/authen.api.go
@@ -18,17 +18,21 @@ func SetupAuthenAPI(router *gin.Engine) {
 }
 
 func login(c *gin.Context) {
-	c.JSON(200, gin.H{"result": "login"})
-}
+	var user model.User
 
-func checkPasswordHash(password, hash string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
-}
+	if c.ShouldBind(&user) == nil {
+		var queryUser model.User		
+		if err := db.GetDB().First(&queryUser, "username = ?", user.Username).Error; err != nil {							
+			c.JSON(200, gin.H{"result": "nok", "error": err})
+		}else if (checkPasswordHash(user.Password, queryUser.Password) == false){
+			c.JSON(200, gin.H{"result": "nok", "error": "invalid password"})
+		}else{			
+			c.JSON(200, gin.H{"result": "ok", "data": user})
+		}
 
-func hashPassword(password string) (string, error) {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-    return string(bytes), err
+	} else {
+		c.JSON(401, gin.H{"status": "unable to bind data"})
+	}	
 }
 
 func register(c *gin.Context) {
@@ -44,4 +48,14 @@ func register(c *gin.Context) {
 	} else {
 		c.JSON(401, gin.H{"status": "unable to bind data"})
 	}
+}
+
+func checkPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+func hashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
 }

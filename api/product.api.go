208b7522f7abdf878569d7dadb3cfda8f611fdd3
Whachara nuanlong <whachara479@gmail.com>
2021-10-13 05:28:48 +0700

    ep11 - Interceptor

--- api/product.api.go
+++ api/product.api.go
@@ -1,11 +1,24 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)		
+
+func myInterceptor(c *gin.Context){
+	token := c.Query("token")
+	if (token == "1234"){
+		c.Next()
+	}else{
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+		c.Abort()
+	}
+}
 
 func SetupProductAPI(router *gin.Engine) {
 	productAPI := router.Group("/api/v2")
 	{
-		productAPI.GET("/product", getProduct)
+		productAPI.GET("/product",myInterceptor, getProduct)
 		productAPI.POST("/product", createProduct)
 	}
 }
